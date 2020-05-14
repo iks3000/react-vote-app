1517f2715039ccc1e0bf61c762c04bd690788dcf
Stanislav <stanislav.khomyn@skylum.com>
2020-05-14 08:53:03 -0700

    Updates

---
+++ precache-manifest.db00b0ca5bf31002c4c63c0fc2829645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58be2676758dd76ce38cfd81fdef7aa3",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "eb488b9e1533e479378f",
+    "url": "/react-movie-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0e730f6550270e6a2b88",
+    "url": "/react-movie-app/static/css/main.7aa5b42c.chunk.css"
+  },
+  {
+    "revision": "eb488b9e1533e479378f",
+    "url": "/react-movie-app/static/js/2.51cad40c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.51cad40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e730f6550270e6a2b88",
+    "url": "/react-movie-app/static/js/main.89fe3f00.chunk.js"
+  },
+  {
+    "revision": "b990e684c578c15f7041",
+    "url": "/react-movie-app/static/js/runtime-main.343acaf3.js"
+  }
+]);
